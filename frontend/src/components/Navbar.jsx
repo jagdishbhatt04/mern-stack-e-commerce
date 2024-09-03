79cd21045e938689f7d2c157d0d9a1cb31345635
HARYANA <Rohittt02>
2024-09-03 15:46:16 +0530

    add navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,13 +1,32 @@
-import React from 'react'
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHeart } from '@fortawesome/free-regular-svg-icons';
+import { faCartShopping, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
+import { faUser as faRegularUser } from '@fortawesome/free-regular-svg-icons';
+import './Navbar.css';
 
 const Navbar = () => {
   return (
-    <div>
-        
-        <h1>Shop heaven</h1>
-
-    </div>
-  )
+    <nav className="navbar">
+      <div className="navbar-content">
+        <div className="navbar-section">
+          <a href="#" className="nav-link">WOMEN</a>
+          <a href="#" className="nav-link">MEN</a>
+          <a href="#" className="nav-link">KIDS</a>
+          <a href="#" className="nav-link">BRANDS</a>
+        </div>
+        <div className="navbar-logo">
+          <a href="#">HEAVENLY</a>
+        </div>
+        <div className="navbar-icons">
+          <a href="#" className="nav-icon"><FontAwesomeIcon icon={faMagnifyingGlass} /></a>
+          <a href="#" className="nav-icon"><FontAwesomeIcon icon={faRegularUser} /></a>
+          <a href="#" className="nav-icon"><FontAwesomeIcon icon={faHeart} /></a>
+          <a href="#" className="nav-icon"><FontAwesomeIcon icon={faCartShopping} /></a>
+        </div>
+      </div>
+    </nav>
+  );
 }
 
-export default Navbar
+export default Navbar;
